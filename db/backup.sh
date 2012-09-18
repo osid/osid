a59b4edcc6831651466d3ad37ff3b3d8d44283e0
osid <opensecurityincidentdb@gmail.com>
2012-09-18 06:55:44 +0200

    added zip and move to home directory

--- db/backup.sh
+++ db/backup.sh
@@ -2,9 +2,15 @@
 NOW=$(date +"%Y%m%d")
 DIR="/home/nik/rails/osidb/db/backup/"
 DB="osidb"
-PWD="DB-password"
+PWD="your-db-password"
 
-mysqldump -t $DB incidents events sources -u root -p$PWD > "$DIR$DB-data-backup-$NOW.sql"
-mysqldump -t $DB users -u root -p$PWD > "$DIR$DB-users-backup-$NOW.sql"
-mysqldump -t $DB countries pvsectors attacks attackers targets -u root -p$PWD > "$DIR$DB-static-backup-$NOW.sql"
-mysqldump --opt $DB -u root -p$PWD > "$DIR$DB-full-backup-$NOW.sql"
+mysqldump -t $DB incidents events sources -u osidb -p$PWD > "$DIR$DB-data-backup-$NOW.sql"
+mysqldump -t $DB users -u osidb -p$PWD > "$DIR$DB-users-backup-$NOW.sql"
+mysqldump -t $DB countries pvsectors attacks attackers targets -u osidb -p$PWD > "$DIR$DB-static-backup-$NOW.sql"
+mysqldump --opt $DB -u osidb -p$PWD > "$DIR$DB-full-backup-$NOW.sql"
+
+cd $DIR
+
+tar -czvf osidb-backup-$NOW.sql.tgz *$NOW.sql
+mv osidb-backup-$NOW.sql.tgz /home/nik/backup
+rm *$NOW.sql
