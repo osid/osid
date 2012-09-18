b03045dcdc280dca4dbd4c4077e285214b76c27a
osid <opensecurityincidentdb@gmail.com>
2012-09-18 13:09:30 +0200

    added mail support of backup

--- db/backup.sh
+++ db/backup.sh
@@ -2,7 +2,7 @@
 NOW=$(date +"%Y%m%d")
 DIR="/home/nik/rails/osidb/db/backup/"
 DB="osidb"
-PWD="your-db-password"
+PWD="DawNeydsojzukthecs"
 
 mysqldump -t $DB incidents events sources -u osidb -p$PWD > "$DIR$DB-data-backup-$NOW.sql"
 mysqldump -t $DB users -u osidb -p$PWD > "$DIR$DB-users-backup-$NOW.sql"
@@ -12,5 +12,6 @@ mysqldump --opt $DB -u osidb -p$PWD > "$DIR$DB-full-backup-$NOW.sql"
 cd $DIR
 
 tar -czvf osidb-backup-$NOW.sql.tgz *$NOW.sql
+mpack -s "OSIDB backup of $NOW"  osidb-backup-$NOW.sql.tgz opensecurityincidentdb@gmail.com
 mv osidb-backup-$NOW.sql.tgz /home/nik/backup
 rm *$NOW.sql
